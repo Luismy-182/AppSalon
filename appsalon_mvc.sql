67ec598d2357e5b3fad7a01b62450e43a902ab7b
luismy <miguel.buny@gmail.com>
2024-10-11 01:39:29 -0600

    se subio el sql completo de la bd

--- appsalon_mvc.sql
+++ appsalon_mvc.sql
@@ -1,52 +1,44 @@
--- -------------------------------------------------------------
--- -------------------------------------------------------------
--- TablePlus 1.2.0
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- https://tableplus.com/
+-- Host: localhost
+-- Generation Time: Oct 11, 2024 at 09:34 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Database: mysql
--- Generation Time: 2024-10-10 22:51:56.432359
--- -------------------------------------------------------------
+-- Database: `appsalon_mvc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `citas`
+--
 
 CREATE TABLE `citas` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `fecha` date NOT NULL,
   `hora` time NOT NULL,
-  `usuarioId` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `usuarioId_FK` (`usuarioId`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `usuarioId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `citasservicios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `citaId` int(11) NOT NULL,
-  `servicioId` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `citaID_FK` (`citaId`),
-  KEY `servicioID_FK` (`servicioId`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+-- Dumping data for table `citas`
+--
 
-CREATE TABLE `servicios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(60) NOT NULL,
-  `precio` decimal(5,2) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(60) NOT NULL,
-  `apellido` varchar(60) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `password` varchar(65) NOT NULL,
-  `telefono` varchar(10) NOT NULL,
-  `admin` tinyint(1) NOT NULL,
-  `confirmado` tinyint(1) NOT NULL,
-  `token` varchar(15) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-INSERT INTO `appsalon_mvc`.`citas` (`id`, `fecha`, `hora`, `usuarioId`) VALUES 
+INSERT INTO `citas` (`id`, `fecha`, `hora`, `usuarioId`) VALUES
 (18, '2024-06-17', '13:00:00', 20),
 (19, '2024-06-19', '14:10:00', 22),
 (21, '2024-10-18', '16:33:00', 30),
@@ -54,7 +46,23 @@ INSERT INTO `appsalon_mvc`.`citas` (`id`, `fecha`, `hora`, `usuarioId`) VALUES
 (23, '2024-10-24', '16:33:00', 30),
 (24, '2024-10-22', '16:00:00', 30);
 
-INSERT INTO `appsalon_mvc`.`citasservicios` (`id`, `citaId`, `servicioId`) VALUES 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `citasservicios`
+--
+
+CREATE TABLE `citasservicios` (
+  `id` int(11) NOT NULL,
+  `citaId` int(11) NOT NULL,
+  `servicioId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `citasservicios`
+--
+
+INSERT INTO `citasservicios` (`id`, `citaId`, `servicioId`) VALUES
 (3, 18, 1),
 (4, 18, 2),
 (5, 18, 4),
@@ -76,7 +84,23 @@ INSERT INTO `appsalon_mvc`.`citasservicios` (`id`, `citaId`, `servicioId`) VALUE
 (21, 23, 16),
 (22, 24, 16);
 
-INSERT INTO `appsalon_mvc`.`servicios` (`id`, `nombre`, `precio`) VALUES 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `servicios`
+--
+
+CREATE TABLE `servicios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `precio` decimal(5,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `servicios`
+--
+
+INSERT INTO `servicios` (`id`, `nombre`, `precio`) VALUES
 (1, 'Corte de Cabello Mujer', 90.00),
 (2, 'Corte de Cabello Hombre', 80.00),
 (3, 'Corte de cabello niño', 60.00),
@@ -90,7 +114,92 @@ INSERT INTO `appsalon_mvc`.`servicios` (`id`, `nombre`, `precio`) VALUES
 (12, 'Tratamiento Capilar', 150.00),
 (16, ' Corte Two Block', 65.00);
 
-INSERT INTO `appsalon_mvc`.`usuarios` (`id`, `nombre`, `apellido`, `email`, `password`, `telefono`, `admin`, `confirmado`, `token`) VALUES 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `apellido` varchar(60) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(65) NOT NULL,
+  `telefono` varchar(10) NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  `confirmado` tinyint(1) NOT NULL,
+  `token` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `email`, `password`, `telefono`, `admin`, `confirmado`, `token`) VALUES
 (30, ' Mike', 'Suarez', 'correo@gmail.com', '$2y$10$UOGcbnzBGpuoWdFrVPPyyOlWlxNDx0Qw5Ue/l9Lz7Wyxgg48JaJSC', '2461735434', 0, 1, ''),
 (31, ' Miguel Angel', 'Suarez', 'admin@hotmail.com', '$2y$10$/tGqXVTw14ZVBrmG8bqrz.4q9mTQMbJ3IZo1Jz5ehhgZsl1hs5SRu', '2461735434', 1, 1, '');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `citas`
+--
+ALTER TABLE `citas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuarioId_FK` (`usuarioId`);
+
+--
+-- Indexes for table `citasservicios`
+--
+ALTER TABLE `citasservicios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `citaID_FK` (`citaId`),
+  ADD KEY `servicioID_FK` (`servicioId`);
+
+--
+-- Indexes for table `servicios`
+--
+ALTER TABLE `servicios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `citas`
+--
+ALTER TABLE `citas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `citasservicios`
+--
+ALTER TABLE `citasservicios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `servicios`
+--
+ALTER TABLE `servicios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
